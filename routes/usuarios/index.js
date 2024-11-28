2258c3d59c736473c1b0cd82d19517f6ea597c27
JoseOlvera2404 <joseolveragarcia24@gmail.com>
2024-11-27 20:15:57 -0600

    Agregamos Ruta Historial

--- routes/usuarios/index.js
+++ routes/usuarios/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import loginRoutes from './login.js';
 import registerRoutes from './register.js';
+import historialRoutes from './historial.js';
 
 const router = express.Router();
 
 //Montar rutas específicas
 router.use('/login', loginRoutes);
 router.use('/register', registerRoutes);
+router.use('/historial', historialRoutes);
 
 export default router;
